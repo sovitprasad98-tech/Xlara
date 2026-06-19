fed99439d9cca0374978f53135af7b7fce4c9ca3
sovitprasad98-tech <sovitprasad98@gmail.com>
2026-06-19 16:11:44 +0530

    Create firebase_config.py

---
+++ api/webhook/firebase_config.py
@@ -0,0 +1,11 @@
+# Firebase config as a separate file
+FIREBASE_CONFIG = {
+    "apiKey": "AIzaSyDrCeKnBujSGLdUXDTLdR0WftXXv6PU7vA",
+    "authDomain": "mines-game-2f39b.firebaseapp.com",
+    "databaseURL": "https://mines-game-2f39b-default-rtdb.firebaseio.com",
+    "projectId": "mines-game-2f39b",
+    "storageBucket": "mines-game-2f39b.firebasestorage.app",
+    "messagingSenderId": "474228392974",
+    "appId": "1:474228392974:web:ee6e646692f40dd01de85f",
+    "measurementId": "G-5PS1C7C5Y9"
+}
